8be8dddd669aca091ee5a25b53c7f3ba80617622
gemmabcr <77388323+gemmabcr@users.noreply.github.com>
2022-10-06 23:47:25 +0200

    exercici 1: get joke function - API

---
+++ index.js
@@ -0,0 +1,17 @@
+async function getDatos() {
+  const API_URL = 'https://icanhazdadjoke.com/';
+  const HTMLResponse = document.getElementById('jokeText');
+
+  fetch(API_URL, {
+    method: 'GET',
+    headers: { 'Accept': 'application/json'
+    }})
+    .then(response => response.json())
+    .then(json => { HTMLResponse.innerText = json.joke; })
+}
+
+
+
+
+
+
