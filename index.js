aad861c5cd1004a205492f914d2391088568120e
gemmabcr <77388323+gemmabcr@users.noreply.github.com>
2022-10-08 17:26:29 +0200

    feat: added second joke api

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 let joke;
 const reportJokes = []
+const JokeHTMLResponse = document.getElementById('jokeText');
+
+function chooseApi(){
+  let random = Math.round(Math.random() * 10);
+  if (random % 2 === 0) getDatos();
+  else getDatos2();
+}
 
 async function getDatos() {
-  const API_URL = 'https://icanhazdadjoke.com/';
-  const HTMLResponse = document.getElementById('jokeText');
+  const JOKE_API_URL = 'https://icanhazdadjoke.com/';
 
-  fetch(API_URL, {
+  fetch(JOKE_API_URL, {
     method: 'GET',
     headers: { 'Accept': 'application/json'
     }})
     .then(response => response.json())
     .then(json => {
-      HTMLResponse.innerText = `"${json.joke}"`;
+      JokeHTMLResponse.innerText = `"${json.joke}"`;
+      document.getElementById('scoreReviewContainer').style.visibility = 'visible';
+      joke = json.joke;
+    })
+}
+
+async function getDatos2() {
+  const JOKE2_API_URL = 'https://v2.jokeapi.dev/joke/Any?lang=es&blacklistFlags=religious,racist,sexist&type=single';
+
+  fetch(JOKE2_API_URL, { method: 'GET' })
+    .then(response => response.json())
+    .then(json => {
+      JokeHTMLResponse.innerText = `"${json.joke}"`;
       document.getElementById('scoreReviewContainer').style.visibility = 'visible';
       joke = json.joke;
     })
